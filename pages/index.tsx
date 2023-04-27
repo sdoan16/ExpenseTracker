6b834dcac48cd18b255981978bee34906f3c3e54
Shirley Doan <shirleydoan@Shirleys-MacBook-Pro.local>
2023-04-27 13:03:13 -0700

    Initial commit from Create Next App

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import React from 'react';
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function ExpenseTracker() {
+  return (
+    <div>Expense Tracker</div>
+  )
+}
